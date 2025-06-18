6ae4b54269b95d941e4bf8a642c7f22584969151
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-18 09:13:32 +0200

    feat: Add scripts for running jobs

---
+++ remote-run-job.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+input_workspace="$1"
+upload_dir="$2"
+
+job_name="$(basename "$upload_dir")"
+
+output_workspace="${input_workspace}_${job_name}_out"
+work_workspace="${input_workspace}_${job_name}_work"
+
+ws_allocate "$output_workspace" 7
+ws_allocate "$work_workspace" 1
+
+cd "$upload_dir"
+slurm_script="$(ls ./*.slurm)"
+sbatch "$slurm_script"
---
+++ run-job.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+if [ $# != 2 ]; then
+  >&2 echo "Usage:   $0 <input_workspace> <slurm_script>"
+  >&2 echo "Example: $0 reproin sample-job-script.slurm"
+  exit 1
+fi
+
+SSH_HOST=marvin
+
+input_workspace="$1"
+slurm_script="$2"
+
+upload_dir="jobs/job_$(date +%Y%m%d_%H%M%S)"
+
+ssh marvin [ -d tools ] || git clone https://github.com/CENsBonn/tools
+ssh marvin mkdir -p "$upload_dir"
+ssh marvin rm -f "jobs/latest"
+ssh marvin ln -s "$(basename "$upload_dir")" "jobs/latest"
+rsync -av --info=progress2 "$slurm_script" "${SSH_HOST}:~/${upload_dir}"
+ssh marvin ./tools/remote-run-job.sh "$input_workspace" "$upload_dir"
