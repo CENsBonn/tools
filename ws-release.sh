c03b01736bc24f053e12ea8dfb8f613e2f9ae5ad
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-18 10:03:26 +0200

    feat: Add script for releasing workspaces

---
+++ ws-release.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+SSH_HOST=marvin
+
+script_dir="$(dirname "$0")"
+
+workspaces="$(ssh "$SSH_HOST" ws_list | "${script_dir}/ws_list_to_json.py" | jq -r "keys.[]")"
+
+selected_workspaces="$(echo "$workspaces" | fzf --multi)"
+
+if [[ -z "$selected_workspaces" ]]; then
+	echo "No workspaces selected. Exiting."
+	exit 0
+fi
+
+echo "Selected workspaces:"
+echo "$selected_workspaces"
+read -r -p "Release these workspaces? [y/n]" confirm
+
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+	for workspace in $selected_workspaces; do
+		ssh "$SSH_HOST" "ws_release '$workspace'"
+	done
+else
+	echo "Aborted."
+fi
