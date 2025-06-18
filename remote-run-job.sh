ed76ec30016d181cade1afd88e2630ef436c3868
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-18 11:46:42 +0200

    feat: Pass fewer params

--- remote-run-job.sh
+++ remote-run-job.sh
@@ -27,4 +27,4 @@ ln -s "$input_dir" input
 ln -s "$output_dir" output
 ln -s "$work_dir" work
 slurm_script="$(ls ./*.slurm)"
-sbatch "$slurm_script" "$input_dir" "$output_dir" "$work_dir" ${@:3}
+sbatch "$slurm_script" ${@:3}
