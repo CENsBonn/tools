2f342d847a3ed761c797a502d7407738e3337175
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-20 09:43:47 +0200

    feat: Add rsync wrapper

---
+++ rsync.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+if [[ $# -ne 2 ]]; then
+  >&2 echo "Download or upload a file or directory using rsync."
+  >&2 echo "Usage:   $0 <source> <destination>"
+  >&2 echo "Example: $0 marvin:~/my_remote_file ."
+  >&2 echo "Example: $0 my_local_file.txt marvin:~/"
+  exit 1
+fi
+
+source="$1"
+destination="$2"
+
+rsync -av --info=progress2 "$source" "$destination"
