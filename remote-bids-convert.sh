f5c861d7e19dfd6744315f03d99f97f810710fec
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-23 11:43:33 +0200

    fix: Softcode script

--- remote-bids-convert.sh
+++ remote-bids-convert.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
+set -Eeuo pipefail
+
 get_workspace_directory() {
 	workspace="$1"
 	python3 -c "import sys, json; data=json.load(sys.stdin); print(data[\"${workspace}\"][\"workspace_directory\"])"
 }
 
-input_workspace="social-detection-7t-bids"
-output_dir="/lustre/scratch/data/sebelin2_hpc-social-detection-7t-bids/"
+input_workspace="$1"
+output_workspace="$2"
 
 ws_allocate "$input_workspace" 50
 
 input_dir="$(ws_list | "$HOME/tools/ws_list_to_json.py" | get_workspace_directory "$input_workspace")"
+output_dir="$(ws_list | "$HOME/tools/ws_list_to_json.py" | get_workspace_directory "$output_workspace")"
 
 heudiconv \
-	--files "${input_dir}/sub-130/*.dcm" \
+	--files "${input_dir}/**/*.dcm" \
 	-s 130 \
 	--bids \
 	-f heuristics/7t.py \
