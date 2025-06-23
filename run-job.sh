c403e7dfdc7534e93bc1ac93e9aacb52331fbd80
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-23 10:22:53 +0200

    refactor: Softcode ssh host

--- run-job.sh
+++ run-job.sh
@@ -13,7 +13,7 @@ SSH_HOST=marvin
 input_workspace="$1"
 slurm_script="$2"
 
-if ! ssh marvin ws_list | ./ws_list_to_json.py | jq -e "has(\"${input_workspace}\")" >/dev/null; then
+if ! ssh "$SSH_HOST" ws_list | ./ws_list_to_json.py | jq -e "has(\"${input_workspace}\")" >/dev/null; then
   >&2 echo "Workspace '${input_workspace}' does not exist. Exiting."
   exit 1
 fi
@@ -21,10 +21,10 @@ fi
 random_str="$(head /dev/urandom | tr -dc 'a-zA-Z0-9' | head -c4)"
 upload_dir="jobs/job_$(date +%Y%m%d_%H%M%S)_${random_str}"
 
-ssh marvin "[ -d tools ] || git clone https://github.com/CENsBonn/tools"
-ssh marvin mkdir -p "$upload_dir"
+ssh "$SSH_HOST" "[ -d tools ] || git clone https://github.com/CENsBonn/tools"
+ssh "$SSH_HOST" mkdir -p "$upload_dir"
 echo "Created job directory: $upload_dir"
-ssh marvin rm -f "jobs/latest"
-ssh marvin ln -s "$(basename "$upload_dir")" "jobs/latest"
+ssh "$SSH_HOST" rm -f "jobs/latest"
+ssh "$SSH_HOST" ln -s "$(basename "$upload_dir")" "jobs/latest"
 rsync -av --info=progress2 "$slurm_script" "${SSH_HOST}:~/${upload_dir}"
-ssh marvin ./tools/remote-run-job.sh "$input_workspace" "$upload_dir" "${@:3}"
+ssh "$SSH_HOST" ./tools/remote-run-job.sh "$input_workspace" "$upload_dir" "${@:3}"
