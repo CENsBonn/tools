6cf450884c0cd0597018372d34866cd6324ce96c
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-25 09:43:02 +0200

    fix: Sort get-ws-dir by creation date

--- get-ws-dir.sh
+++ get-ws-dir.sh
@@ -9,7 +9,7 @@ ws_list_output="$(ssh "$SSH_HOST" ws_list -C | ./ws_list_to_json.py)"
 selected_ws="${1:-}"
 
 if [ -z "$selected_ws" ]; then
-  selected_ws="$(echo "$ws_list_output" | jq -r 'keys[]' | fzf --no-sort --tac)"
+  selected_ws="$(echo "$ws_list_output" | jq -r 'to_entries[] | .key' | fzf --no-sort --tac)"
 fi
 
 ws_dir="$(echo "$ws_list_output" | jq -r --arg ws "$selected_ws" '.[$ws].workspace_directory')"
