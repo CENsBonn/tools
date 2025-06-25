aaa85030dc89620fd2da65253150958bef203bc2
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-25 09:35:04 +0200

    fix: Make list-ws output workspace in creation order properly

--- list-ws.sh
+++ list-ws.sh
@@ -4,4 +4,6 @@ set -Eeuo pipefail
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-ssh marvin ws_list -C | "${script_dir}/ws_list_to_json.py" | jq -r 'keys[]'
+ssh marvin ws_list -C \
+    | "${script_dir}/ws_list_to_json.py" \
+    | LC_TIME=C jq -r 'to_entries[] | .key as $key | .value.creation_time as $ct | $ct | strptime("%a %b %e %H:%M:%S %Y") | mktime as $ts | "\($ts) \($key)"'
