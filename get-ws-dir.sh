6090d19c8d4ab6f04701748c6d405d38dda25552
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-25 09:33:30 +0200

    feat: Support optional argument in get-ws-dir

--- get-ws-dir.sh
+++ get-ws-dir.sh
@@ -5,7 +5,13 @@ set -Eeuo pipefail
 SSH_HOST=marvin
 
 ws_list_output="$(ssh "$SSH_HOST" ws_list -C | ./ws_list_to_json.py)"
-selected_ws="$(echo "$ws_list_output" | jq -r 'keys[]' | fzf --no-sort --tac)"
+
+selected_ws="${1:-}"
+
+if [ -z "$selected_ws" ]; then
+  selected_ws="$(echo "$ws_list_output" | jq -r 'keys[]' | fzf --no-sort --tac)"
+fi
+
 ws_dir="$(echo "$ws_list_output" | jq -r --arg ws "$selected_ws" '.[$ws].workspace_directory')"
 
 echo "$ws_dir"
