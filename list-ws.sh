6e5cbe03d20f83167c363a1a4dbbab1233761709
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-06-25 09:38:03 +0200

    fix: Simplify list-ws

--- list-ws.sh
+++ list-ws.sh
@@ -4,6 +4,4 @@ set -Eeuo pipefail
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
-ssh marvin ws_list -C \
-    | "${script_dir}/ws_list_to_json.py" \
-    | LC_TIME=C jq -r 'to_entries[] | .key as $key | .value.creation_time as $ct | $ct | strptime("%a %b %e %H:%M:%S %Y") | mktime as $ts | "\($ts) \($key)"'
+ssh marvin ws_list -C | ./ws_list_to_json.py | jq -r 'to_entries[].key'
