a79343fedde50b6d35e88ffa1ca808c6a21aeb67
Ville Sebastian Olsson <sebelino7@gmail.com>
2025-07-07 15:17:23 +0200

    fix: Support optional argument for release-ws.sh as advertised

--- release-ws.sh
+++ release-ws.sh
@@ -6,13 +6,16 @@ SSH_HOST=marvin
 
 script_dir="$(dirname "$0")"
 
-workspaces="$(ssh "$SSH_HOST" ws_list | "${script_dir}/ws_list_to_json.py" | jq -r "keys.[]")"
-
-selected_workspaces="$(echo "$workspaces" | fzf --multi)"
+if [[ $# -gt 0 ]]; then
+  selected_workspaces="$1"
+else
+  workspaces="$(ssh "$SSH_HOST" ws_list | "${script_dir}/ws_list_to_json.py" | jq -r "keys.[]")"
+  selected_workspaces="$(echo "$workspaces" | fzf --multi)"
+fi
 
 if [[ -z "$selected_workspaces" ]]; then
-	echo "No workspaces selected. Exiting."
-	exit 0
+  echo "No workspaces selected. Exiting."
+  exit 0
 fi
 
 echo "Selected workspaces:"
@@ -20,10 +23,10 @@ echo "$selected_workspaces"
 read -r -p "Release these workspaces? [y/n]" confirm
 
 if [[ "$confirm" =~ ^[Yy]$ ]]; then
-	for workspace in $selected_workspaces; do
-		echo "Releasing workspace '$workspace'..."
-		ssh "$SSH_HOST" "ws_release '$workspace'"
-	done
+  for workspace in $selected_workspaces; do
+    echo "Releasing workspace '$workspace'..."
+    ssh "$SSH_HOST" "ws_release '$workspace'"
+  done
 else
-	echo "Aborted."
+  echo "Aborted."
 fi
